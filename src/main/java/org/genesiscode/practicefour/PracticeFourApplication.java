2b94d496149e884509a3a4b848f7dd4d39e996bd
Jose Maria <josmariaguilar@gmail.com>
2022-09-11 17:08:01 -0400

    Initial Project

---
+++ src/main/java/org/genesiscode/practicefour/PracticeFourApplication.java
@@ -0,0 +1,12 @@
+package org.genesiscode.practicefour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeFourApplication.class, args);
+	}
+}
---
+++ src/test/java/org/genesiscode/practicefour/PracticeFourApplicationTests.java
@@ -0,0 +1,13 @@
+package org.genesiscode.practicefour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
