c3fe33d128e646e94551c0be59b05a7cd0ed349c
Jose Maria <josmariaguilar@gmail.com>
2022-09-11 21:10:45 -0400

    multiplicative implemented

--- src/main/java/org/genesiscode/practicefour/dto/MultiplicativeResponseDTO.java
+++ src/main/java/org/genesiscode/practicefour/dto/MultiplicativeResponseDTO.java
@@ -16,10 +16,4 @@ public class MultiplicativeResponseDTO {
     private int valueTwo;
     private int valueThree;
     private String valueFour;
-
-    public static void main(String[] args) {
-        MultiplicativeResponseDTO row1 = new MultiplicativeResponseDTO(1, 10, 1, 1, "Jose Maria");
-        MultiplicativeResponseDTO row2 = new MultiplicativeResponseDTO(100, 1, 1, 1, "Jose Maria");
-        System.out.println(row1.equals(row2));
-    }
 }
--- src/main/java/org/genesiscode/practicefour/service/impl/MainServiceImpl.java
+++ src/main/java/org/genesiscode/practicefour/service/impl/MainServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.genesiscode.practicefour.dto.MultiplicativeResponseDTO;
 import org.genesiscode.practicefour.dto.ResponseDTO;
 import org.genesiscode.practicefour.service.MainService;
+import org.genesiscode.practicefour.util.Operations;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ import java.util.List;
 public class MainServiceImpl implements MainService {
 
     @Override
-    public ResponseDTO<MultiplicativeResponseDTO> multiplicative(final Integer seed, final Integer multiplicativeConstant, Integer module) {
+    public ResponseDTO<MultiplicativeResponseDTO> multiplicative(final Integer seed, final Integer multiplicativeConstant, final Integer module) {
         List<MultiplicativeResponseDTO> list = new ArrayList<>();
         int seedTemp = seed;
         boolean isFirstIteration = true, isRepeat = false;
@@ -35,10 +36,17 @@ public class MainServiceImpl implements MainService {
             seedTemp = valueThree;
         }
         log.info("multiplicative returns with successfully");
-        return new ResponseDTO<>(list, messagesMultiplicative());
+        return new ResponseDTO<>(list, messagesMultiplicative(seed, module));
     }
 
-    private List<String> messagesMultiplicative() {
-        return new ArrayList<>();
+    private List<String> messagesMultiplicative(int seed, int module) {
+        List<String> messages = new ArrayList<>();
+        if (seed % 2 == 0 || seed % 5 == 0) {
+            messages.add(String.format("Warning: La semilla %s es divisible entre 2 o 5", seed));
+        }
+        if (!Operations.areRelativelyPrime(seed, module)) {
+            messages.add(String.format("Warning: La semilla %s no es relativamente primo al modulo: %s", seed, module));
+        }
+        return messages;
     }
 }
